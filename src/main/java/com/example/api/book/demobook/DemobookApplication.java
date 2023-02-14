9dfefaeb0d1addcf5cca9aff1612e93837f5ecc2
Ashwini <ashwinijadhav1311@gmail.com>
2023-02-14 15:37:25 +0530

    Create demobook

---
+++ src/main/java/com/example/api/book/demobook/DemobookApplication.java
@@ -0,0 +1,14 @@
+package com.example.api.book.demobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobookApplication.class, args);
+		System. out .print ("hello spring");
+	}
+
+}
